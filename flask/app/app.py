df2b0324e320066f5f910e7afb9540954066955e
AbebeAbaba <167415983+AbebeAbaba@users.noreply.github.com>
2025-02-15 15:25:04 +0900

    app.py is changed

--- flask/app/app.py
+++ flask/app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 from . import english
 
 app = Flask(__name__)
@@ -15,6 +15,10 @@ def en():
 def kanji():
     return render_template("kanji.html")
 
-@app.route("/english/enpy")
+@app.route("/english/enpy", methods=["GET", "POST"])
 def enpy():
-    return english.main()
+    if request.method == "POST":
+        user_input = request.form["user_input"]  # フォームからの入力を取得
+        result = english.main(user_input)  # 結果を取得
+        return render_template("english.html", result=result)  # 結果を表示
+    return render_template("english.html")
