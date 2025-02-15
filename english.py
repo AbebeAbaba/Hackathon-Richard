3410ed550cd184406fe9dd00da12620635ee9b28
AbebeAbaba <167415983+AbebeAbaba@users.noreply.github.com>
2025-02-15 09:30:45 +0900

    change all
    change

---
+++ english.py
@@ -0,0 +1,45 @@
+
+def main():
+    prefix = ["anti","de","dis",
+              "en","em","fore",
+              "in","im","il",
+              "ir","inter","mid",
+              "mis","non","over",
+              "pre","re","semi",
+              "sub","super","trans",
+              "un","under"
+              ]
+
+    suffix = ["able","ible","ed",
+              "en","er","est",
+              "ful","ic","ing",
+              "ion","ty","ive",
+              "less","ly","ment",
+              "ness","ous"
+              ]
+    
+    flag = 0
+
+    # 配列として入力受け取り
+    user_input = (input("比較したい文字を入力してください: "))
+
+    # 入力された文字とJSON内の漢字を1文字ずつ比較
+    for pre in prefix:
+        if(user_input.startswith(pre) == True):
+            print(pre)
+            flag = 1
+            break
+
+    for suf in suffix:
+        if(user_input.endswith(suf) == True):
+            print(suf)
+            flag = 1
+            break
+    
+    if(flag == 0):
+        print(f"{user_input} miss")
+
+
+# 実行部分
+if __name__ == "__main__":
+    main()
--- kanji.py
+++ kanji.py
@@ -1,1 +1,57 @@
+import json
 
+
+def main():
+    # JSONファイルのパス
+    # 漢字右左ファイル
+    file_path_lr = 'kanji_left_right.json'
+    # 漢字上下ファイル
+    file_path_tp = 'kanji_top_buttom.json'
+
+    # JSONファイル読み込み
+    try:
+        json_data_lr = load_json(file_path_lr)
+        json_data_tp = load_json(file_path_tp)
+
+    # 読み込み失敗
+    except FileNotFoundError:
+        print("エラー: JSONファイルが見つかりません。")
+        return
+    except json.JSONDecodeError:
+        print("エラー: JSONの形式が正しくありません。")
+        return
+
+    # 配列として入力受け取り
+    user_input = list(input("比較したい文字を入力してください: "))
+
+    # 入力された文字とJSON内の漢字を1文字ずつ比較
+    for kanji in user_input:
+        # 漢字右左分解 成功 -> 0
+        if(compare_with_input(json_data_lr, kanji) != 0):
+            # 漢字上下分解 成功 -> 0
+            if(compare_with_input(json_data_tp, kanji) != 0):
+                # 両方失敗 -> 失敗報告
+                print(f"入力された文字 '{kanji}' はキーに存在しません。")
+
+
+# JSONファイル読み込み関数 漢字リスト
+def load_json(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+# 入力とJSONファイル内の漢字を比較する関数aaa
+def compare_with_input(json_data, kanji):
+    # 1文字ずつ確認する あれば進む
+    if kanji in json_data:
+        # 対応する配列を取得
+        # array_data: 分解後の漢字を含む配列
+        array_data = json_data[kanji]
+        # 結果出力
+        print(f"入力された文字 '{kanji}' はキーに存在します。対応する配列: {array_data}")
+        #成功報告
+        return 0
+
+
+# 実行部分
+if __name__ == "__main__":
+    main()
