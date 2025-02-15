35d4b3e5a80fe2490f98c3d6f359b0de63e35f71
AbebeAbaba <167415983+AbebeAbaba@users.noreply.github.com>
2025-02-15 14:16:53 +0900

    move

--- flask/app/app.py
+++ flask/app/app.py
@@ -1,18 +1,20 @@
 from flask import Flask,render_template
+from . import english
 
 app = Flask(__name__)
 
 @app.route("/")
-
 def index():
     return render_template("index.html")
 
 @app.route("/english")
-
-def english():
+def en():
     return render_template("english.html")
 
 @app.route("/kanji")
-
 def kanji():
     return render_template("kanji.html")
+
+@app.route("/english/enpy")
+def enpy():
+    return english.main()
