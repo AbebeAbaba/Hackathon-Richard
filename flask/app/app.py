9473e891efc7ccd4574325aea93989c27e6039cc
AbebeAbaba <167415983+AbebeAbaba@users.noreply.github.com>
2025-02-15 22:55:39 +0900

    change

--- flask/app/app.py
+++ flask/app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,request
-from . import english,kanji
+from . import english
+from . import kanji
 
 app = Flask(__name__)
 
@@ -12,21 +13,21 @@ def en():
     return render_template("english.html")
 
 @app.route("/kanji")
-def kanji():
+def kn():
     return render_template("kanji.html")
 
 @app.route("/english/enpy", methods=["GET", "POST"])
 def enpy():
     if request.method == "POST":
-        user_input = request.form["user_input"]  # フォームからの入力を取得
-        result_en = english.main_en(user_input)  # 結果を取得
+        user_input_en = request.form["user_input"]  # フォームからの入力を取得
+        result_en = english.main_en(user_input_en)  # 結果を取得
         return render_template("english.html", result=result_en)  # 結果を表示
     return render_template("english.html")
 
 @app.route("/kanji/knpy", methods=["GET", "POST"])
 def knpy():
     if request.method == "POST":
-        user_input = request.form["user_input"]  # フォームからの入力を取得
-        result_kn = kanji.main_kn(user_input)  # 結果を取得
+        user_input_kn = request.form["user_input"]  # フォームからの入力を取得
+        result_kn = kanji.main_kn(user_input_kn)  # 結果を取得
         return render_template("kanji.html", result=result_kn)  # 結果を表示
     return render_template("kanji.html")
