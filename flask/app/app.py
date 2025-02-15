c881a35d0a14d322eb1e75a9f19d558881bece7e
AbebeAbaba <167415983+AbebeAbaba@users.noreply.github.com>
2025-02-15 11:47:12 +0900

    遷移完了

--- flask/app/app.py
+++ flask/app/app.py
@@ -6,3 +6,13 @@ app = Flask(__name__)
 
 def index():
     return render_template("index.html")
+
+@app.route("/english")
+
+def english():
+    return render_template("english.html")
+
+@app.route("/kanji")
+
+def kanji():
+    return render_template("kanji.html")
